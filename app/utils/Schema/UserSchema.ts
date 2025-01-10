4ff4c70944b196fbf7071cdcbfa5314ded97a5f7
sananbsd2024 <sananbsd2024@gmail.com>
2025-01-10 17:27:59 +0700

    Update UserSchema.ts

--- app/utils/Schema/UserSchema.ts
+++ app/utils/Schema/UserSchema.ts
@@ -10,8 +10,6 @@ const UserSchema = new mongoose.Schema({
   createdAt: { type: Date, default: Date.now },
 });
 
-// Check if the models are already registered, otherwise define them
 const User = mongoose.models.User || mongoose.model("User", UserSchema);
 
-// Use named exports
 export default User;
